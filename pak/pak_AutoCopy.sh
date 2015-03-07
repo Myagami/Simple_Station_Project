71ed944c2178efa2bbb423a678e60cbd589c9776
M.Yagami <helloproject.odoru10th@gmail.com>
2015-03-07 23:43:35 +0900

    Update for 15/03/07 23:43:35

--- pak/pak_AutoCopy.sh
+++ pak/pak_AutoCopy.sh
@@ -3,6 +3,6 @@
 
 ## 全体のモニター
 while inotifywait -e modify,create,delete -r ./ ; do
-    cp "*.pak" /home/hayate/simutrans/addons/pak.nippon.test/
+    `cp *.pak /home/hayate/simutrans/addons/pak.nippon.test/`
 done
 
