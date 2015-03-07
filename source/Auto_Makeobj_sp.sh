a96a41ae9eb78c02a1c99b31b99b535c7f029fa4
M.Yagami <helloproject.odoru10th@gmail.com>
2015-03-07 23:47:03 +0900

    Update for 15/03/07 23:47:03

--- source/Auto_Makeobj_sp.sh
+++ source/Auto_Makeobj_sp.sh
@@ -7,9 +7,7 @@ inotifywait -m -e modify,create,delete --format %w%f $PNG $DAT|
 #inotifywait -m -e modify,create,delete --format %w%f $DAT|
 while read files;do
     makeobj_54 pak ./ $DAT ;
-    if [${DAT} = "./"] ;then
-	cp `*.pak ~/simutrans/addons/pak.nippon.test`
-    fi
+    cp `*.pak ~/simutrans/addons/pak.nippon.test`
     echo "--------------------------------"
     date +"%y/%m/%d %T";
     echo "--------------------------------"
