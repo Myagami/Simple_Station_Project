279897ceae95df38903cd3da28c0c1766b06aaab
M.Yagami <helloproject.odoru10th@gmail.com>
2015-03-07 23:40:22 +0900

    Update for 15/03/07 23:40:22

--- source/Auto_Makeobj_Pr.sh
+++ source/Auto_Makeobj_Pr.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DAT=$1
 ## png / datをモニターする
-inotifywait -m -e modify,create,delete --format %w%f "*.png" $DAT|
+inotifywait -m -e modify,create,delete --format %w%f '*.png' $DAT|
 #inotifywait -m -e modify,create,delete --format %w%f $DAT|
 while read files;do
     makeobj_54 pak "../pak/" $DAT ;
