078db80f436cb7b0b2d74f3ad9accee47eed045d
M.Yagami <helloproject.odoru10th@gmail.com>
2015-03-09 10:31:16 +0900

    Update for 15/03/09 10:31:16

--- sh/pak_AutoCopy.sh
+++ sh/pak_AutoCopy.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 #PAK=$1
 ## png / datをモニターする
-#inotifywait -m -e modify,create,delete --format %w%f "./" |
-inotifywait -m close_nowrite --format %w%f "./" |
+inotifywait -m -e modify,create,delete --format %w%f "./" |
 #inotifywait -m -e modify,create,delete --format %w%f $DAT|
 while read files;do
 
