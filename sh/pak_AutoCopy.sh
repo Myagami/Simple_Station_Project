0fdd288cc2b3bc97044b06d3a41b8303984009fc
M.Yagami <helloproject.odoru10th@gmail.com>
2015-03-09 10:29:08 +0900

    Update for 15/03/09 10:29:08

--- sh/pak_AutoCopy.sh
+++ sh/pak_AutoCopy.sh
@@ -4,11 +4,12 @@
 inotifywait -m -e modify,create,delete,attrib --format %w%f "./" |
 #inotifywait -m -e modify,create,delete --format %w%f $DAT|
 while read files;do
-    echo "--------------------------------"
-    date +"%y/%m/%d %T";
-    echo "--------------------------------"
 
     cp * ~/simutrans/addons/pak.nippon.test/
     #/home/hayate/simutrans/simutrans.start.112-3-Dev.sh
+
+    echo "--------------------------------"
+    date +"%y/%m/%d %T";
+    echo "--------------------------------"
 done
 
