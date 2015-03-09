f350b6e544bd6e66e360b80fb876fc306dc8ea7d
M.Yagami <helloproject.odoru10th@gmail.com>
2015-03-09 10:30:48 +0900

    Update for 15/03/09 10:30:48

--- sh/pak_AutoCopy.sh
+++ sh/pak_AutoCopy.sh
@@ -2,7 +2,7 @@
 #PAK=$1
 ## png / datをモニターする
 #inotifywait -m -e modify,create,delete --format %w%f "./" |
-inotifywait -m CLOSE_NOWRITE --format %w%f "./" |
+inotifywait -m close_nowrite --format %w%f "./" |
 #inotifywait -m -e modify,create,delete --format %w%f $DAT|
 while read files;do
 
