47fdc684a84ca48e2f65af8d0bc71c62b26fa03a
M.Yagami <helloproject.odoru10th@gmail.com>
2015-03-09 09:45:42 +0900

    Update for 15/03/09 09:45:42

--- sh/png_Update.sh
+++ sh/png_Update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-## png / datをモニターする
+## pngが更新されたらimage_upを更新する
 inotifywait -m -e modify,create,delete --format %w%f "./"|
 #inotifywait -m -e modify,create,delete --format %w%f $DAT|
 while read files;do
