d08934248f4cab88400a1d1b5fca75e90b425dc8
M.Yagami <helloproject.odoru10th@gmail.com>
2015-03-09 10:34:12 +0900

    Update for 15/03/09 10:34:12

--- sh/pak_AutoCopy.sh
+++ sh/pak_AutoCopy.sh
@@ -2,7 +2,7 @@
 #PAK=$1
 ## png / datをモニターする
 #inotifywait -m -e close_nowrite,create,delete --format %w%f "./" |
-inotifywait -m -e modify,create,delete --format %w%f "./|"
+inotifywait -m -e modify,create,delete --format %w%f "./"
 while read files;do
 
     cp *.pak ~/simutrans/addons/pak.nippon.test/
