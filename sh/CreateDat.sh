dd80eb0d58bc5a6dd50e329092b197624f2b7aba
M.Yagami <helloproject.odoru10th@gmail.com>
2015-03-13 00:08:05 +0900

    Update for 15/03/13 00:08:05

--- sh/CreateDat.sh
+++ sh/CreateDat.sh
@@ -3,7 +3,7 @@
 # -m マルチタイルアドオン作成 → プロジェクトデータ発行
 # -s シングルタイルアドオン作成 → dat出力
 NAME=$2
-while getopts ms: opt
+while getopts ms opt
 do
     case $opt in
 	"m")echo "multi\n"; sed -e "s/S_Name/$NAME/gi" ../template/Simple_Station_Base.tcp > Simple_Station_$NAME.tcp ;;
