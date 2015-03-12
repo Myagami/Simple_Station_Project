fd151d14ed75456d4801fdba098a4cf1847ddeeb
M.Yagami <helloproject.odoru10th@gmail.com>
2015-03-12 20:32:54 +0900

    Update for 15/03/12 20:32:54

--- sh/CreateDat.sh
+++ sh/CreateDat.sh
@@ -2,12 +2,13 @@
 # Option
 # -m マルチタイルアドオン作成 → プロジェクトデータ発行
 # -s シングルタイルアドオン作成 → dat出力
-while getopts ms: opt 
+NAME=$2
+while getopts ms: opt
 do
     case $opt in
-	"m")echo "multi\n"; sed -e "s/S_Name/$OPTARG/gi" ../common/Simple_Project.tcp > Simple_Station_$OPTARG.tcp ;;
-	#"m") echo "multi\n" ;;
-	"s") echo "single\n" ;sed -e "s/S_Name/$OPTARG/gi" ../common/Simple_Station_Base.dat > Simple_Station_$OPTARG.dat ;;
+	"m")echo "multi\n"; sed -e "s/S_Name/$NAME/gi" ../common/Sample_Project.tcp > Simple_Station_$NAME.tcp ;;
+	#"m") echo "$NAME\n" ;;
+	"s") echo "single\n" ;sed -e "s/S_Name/$NAME/gi" ../common/Simple_Station_Base.dat > Simple_Station_$NAME.dat ;;
 	#"s") echo "single\n" ;;
     esac
 done
