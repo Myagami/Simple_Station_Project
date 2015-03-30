20ae37e974d981f014a2af5dcee6d8517fec3a17
M.Yagami <helloproject.odoru10th@gmail.com>
2015-03-30 22:02:30 +0900

    Update for 15/03/30 22:02:30

--- sh/Makeobj_Support.pl
+++ sh/Makeobj_Support.pl
@@ -109,7 +109,7 @@ sub make_run{#makeobj
     print "\n" ;
     my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime(time);  
     printf("Run:%04d/%02d/%02d %02d:%02d:%02d \n", $year + 1900, $mon + 1, $mday,$hour,$min,$sec);  
-    while($buff = <$fh_r>){
+    while(my $buff = <$fh_r>){
 	chomp($buff) ;
 	if($buff =~ /Makeobj version (.*) for Simutrans (.*) and higher/){#バージョン
 	    print "Makeobj Version:".$1."\n" ;
