176e3d336650931651f30408311c292e5a696f12
M.Yagami <helloproject.odoru10th@gmail.com>
2015-04-03 13:46:02 +0900

    starting

--- sh/Makeobj_Support.pl
+++ sh/Makeobj_Support.pl
@@ -3,11 +3,12 @@ use strict;
 use warnings;
 use Linux::Inotify2;
 use AnyEvent; 
+use File::Copy ;
 #messege
 
-print "--------------------------\n" ;
-print "Makeobj Support Tools\n" ;
-print "--------------------------\n" ;
+print "+---------------------+\n" ;
+print "|Makeobj Support Tools|\n" ;
+print "+---------------------+\n" ;
 
 #Start
 my $watch_dir = "./" ;
@@ -51,9 +52,7 @@ $inotify->watch(
 		print "Can't Running Makeobj\n" ;
 	    }
 	}else{#アイコン
-	    print "Full Build\n" ;
-	    make_run("./") ;
-	    print "Icon_png[${mask}]:${p_name}\n";
+	    print "Update Icon File\n Next edit fire dat or source_png\n" ;
 	}
 	print "------------\n" ;
     }
@@ -99,38 +98,65 @@ sub exist_file{#ファイル確認
 
 sub make_run{#makeobj
     my $fn = shift @_ ;
-
+    print "Update or Create:".$fn."\n" ;
     if($fn =~ /.*png$/){#pngならdatに変える
 	$fn = png_to_dat($fn) ;
     }
-    print $fn."\n" ;
-    open my $fh_r, 'makeobj_54 pak ../pak/ ./${fn} 2>&1|';
+    open my $fh_r, "makeobj_54 pak ../pak/ ${fn} 2>&1|";
     print "-"x15 ;
     print "\n" ;
     my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime(time);  
     printf("Run:%04d/%02d/%02d %02d:%02d:%02d \n", $year + 1900, $mon + 1, $mday,$hour,$min,$sec);  
+    
+    my $f_path = 0 ;#ファイルパス
+    my $f_name = 0 ;#ファイルネーム
+    my $f_cp = 0 ;#作業フラグ
+    
+    #データ解析
     while(my $buff = <$fh_r>){
 	chomp($buff) ;
+	$f_cp = 1 ;
 	if($buff =~ /Makeobj version (.*) for Simutrans (.*) and higher/){#バージョン
 	    print "Makeobj Version:".$1."\n" ;
 	    print "Support Main:".$2."\n" ;
 	}elsif($buff =~ /writing invidual files to (.*)/){#書き込み先
 	    print "Write File path:".$1."\n" ;
+	    $f_path = $1 ;
 	}elsif($buff =~ /   writing file (.*)/){
-	    print "Write File to:".$1."\n" ;
+	    $f_name = $1 ;
+	    print "Write File to:".$f_name."\n" ;
+	    pak_data_copy($f_name,$f_path) ;
 	}elsif($buff =~ /writing file (.*)/){
-	    print "Write File to:".$1."\n" ;
+	    $f_name = $1 ;
+	    print "Write File to:".$f_name."\n" ;
+	    pak_data_copy($f_name,$f_path) ;
 	}elsif($buff =~ /WARNING: (.*)$/){#危険
 	    print "Warning:".$1."\n" ;
 	}elsif($buff =~ /reading file (.*)$/){#危険
 	    print "Read From:".$1."\n" ;
-	}elsif($buff =~ /packing (.*)$/){#危険
+	}elsif($buff =~ /packing (.*)$/){#packing
 	    print "Packd Data:".$1."\n" ;
 	}elsif($buff =~ /Error:: (.*)$/){#危険
 	    print "Error:".$1."-\n" ;
+	    $f_cp = 0 ;
 	}elsif($buff =~ /ERROR IN CLASS (.*): cannot open (.*)/){#危険
 	    print "ErrorClass:${1} - Can't open ${2}\n" ;
+	    $f_cp = 0 ;
 	}
+    }
+}
+
+sub pak_data_copy{#(f_name,f_path)
+    my $f_name = shift @_ ;
+    my $f_name_w = $f_name ;
+    my $f_path = shift @_ ;
+    $f_name_w =~ s/$f_path//g ; 
+    my $f_name_r = copy($f_name,"/home/hayate/simutrans/addons/pak.nippon.test/".$f_name_w) ;
+    if($f_name_r == 1){
+	print "Create pak:".$f_name_w."\n" ;
+	print "Copy成功.\n" ;
+    }else{
+	print "Copy失敗.\n" ;
     }
 }
 
